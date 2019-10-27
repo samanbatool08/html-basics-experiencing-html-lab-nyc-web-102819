94bee55fb84021fb4a39b4c16964ec3b702b2f1c
saman batool <samanbatool08@gmail.com>
2019-10-27 19:36:47 +0000

    Automatically backed up by Learn

--- spec/html_spec.rb
+++ spec/html_spec.rb
@@ -1,8 +1,5 @@
-require_relative './spec_helper'
-
-describe "A populated HTML file called my_list.html" do
-  it 'should be present' do
-    file_is_present = File.exists?("my_list.html")
-    expect(file_is_present).to be_truthy, "Make sure you created a file called my_list.html with your HTML document in it!"
-  end
-end
+<li>"Love Will Tear Us Apart"</li>
+<li>"Bela Lugosi's Dead"</li>
+<li>"A Forest"</li>
+<li>"Nine While Nine"</li>
+<li>"Girlfriend In a Coma"</li>
